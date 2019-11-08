3264ace66ef34e5380fe93c99fe77125e001d964
Mikhail Ivanov <ivmikhail@gmail.com>
2019-11-09 01:52:46 +0300

    Added sonarqube

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,12 @@ allprojects {
         jcenter()
     }
 }
+plugins {
+    id("org.sonarqube") version "2.8"
+}
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "ivmikhail_coursera.org")
+    }
+}
